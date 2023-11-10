9f059fe573495410fd9006928b320d3f904381e6
ValentineNam <v.nam@space307.com>
2023-11-10 22:12:30 +0300

    add step-3 features

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
-'use sctrict'
+'use strict'
+
+import { getName } from '../src/cli.js';
+
+let name = '';
 
 console.log('Welcome to the Brain Games!');
+name = getName();
+console.log(`Hello, ${name}`);
