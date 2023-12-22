e8aa8b93e55910d01f70f053461daf02fe076490
v.nam <v4lentine.nam@gmail.com>
2023-12-23 00:54:40 +0300

    step-8 add progression file

---
+++ bin/brain-progression.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import getUserInput from '../src/cli.js';
+import generateRandomInt from '../src/math-funcs.js';
+import gameLoop from '../src/index.js';
+
+console.log('hello world');
