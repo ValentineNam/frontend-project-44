3f6b91bfab6b09b52676aa10cf337f591a633bff
v.nam <v4lentine.nam@gmail.com>
2023-12-23 00:54:15 +0300

    step-7 fix linter problems

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -5,16 +5,16 @@ import generateRandomInt from '../src/math-funcs.js';
 import gameLoop from '../src/index.js';
 
 const cgdSearch = (a, b) => {
-    let temp;
-    let tempA = a;
-    let tempB = b;
-    while (tempB) {
-        temp = tempB;
-        tempB = tempA % tempB;
-        tempA = temp;
-    }
-    return tempA;
-}
+  let temp;
+  let tempA = a;
+  let tempB = b;
+  while (tempB) {
+    temp = tempB;
+    tempB = tempA % tempB;
+    tempA = temp;
+  }
+  return tempA;
+};
 
 const checkAnswer = (calculated, answer) => (parseInt(calculated, 10) === parseInt(answer, 10));
 
@@ -22,8 +22,8 @@ const game = (inputedName) => {
   const name = inputedName;
   let res = true;
   let calculated = 0;
-  let a = generateRandomInt(1, 150);
-  let b = generateRandomInt(1, 150);
+  const a = generateRandomInt(1, 150);
+  const b = generateRandomInt(1, 150);
   calculated = cgdSearch(a, b);
   console.log(`Find the greatest common divisor of given numbers.\nQuestion: ${a} ${b}`);
   const answer = getUserInput('Your answer: ');
