ca638f91d1a94ad9cbf8b7622b07cfb180f2ea56
v.nam <v4lentine.nam@gmail.com>
2023-12-23 03:16:02 +0300

    fixes after code-review round 3 (or 4? nevermind)

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,16 +1,5 @@
 #!/usr/bin/env node
 
-import getUserInput from '../src/cli.js';
 import gameLoop from '../src/index.js';
-
-const welcome = () => {
-  let name = '';
-  console.log('Welcome to the Brain Games!');
-  name = getUserInput('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
-};
-
-export default welcome;
 
 gameLoop();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import welcome from '../bin/brain-games.js';
+import getUserInput from './cli.js';
+
+export const welcome = () => {
+  let name = '';
+  console.log('Welcome to the Brain Games!');
+  name = getUserInput('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
 
 const gameLoop = (fn) => {
   let isWin = true;
   const name = welcome();
 
   for (let quest = 3; quest > 0 && isWin; quest -= 1) {
-    isWin = fn? fn(name) : false;
+    isWin = fn ? fn(name) : false;
   }
 
   if (isWin) {
